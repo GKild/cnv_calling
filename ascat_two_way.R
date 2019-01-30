aff21f11df793aecbe100a48a9706275e68e3d17
GKild <gk14@sanger.ac.uk>
2019-01-30 12:04:40 +0000

    different output locs

--- ascat_two_way.R
+++ ascat_two_way.R
@@ -15,9 +15,9 @@ write.table(segment_file_ips, args[6], sep="\t", col.names = TRUE, row.names = F
 ascat.bc_fibro_tum <- ascat.loadData(args[3], args[4], args[1], args[2])
 ascat.bc_fibro_tum <-ascat.GCcorrect(ascat.bc_fibro_tum, args[5])
 ascat.plotRawData(ascat.bc_fibro_tum, img.dir = args[7]) 
-ascat.bc_fibro_tum = ascat.aspcf(ascat.bc_fibro_tum, out.dir = args[6])
-ascat.plotSegmentedData(ascat.bc_fibro_tum , img.dir = args[6])
-ascat.output_fibro_tum = ascat.runAscat(ascat.bc_fibro_tum, gamma = 0.55, img.dir = args[6])
+ascat.bc_fibro_tum = ascat.aspcf(ascat.bc_fibro_tum, out.dir = args[7])
+ascat.plotSegmentedData(ascat.bc_fibro_tum , img.dir = args[7])
+ascat.output_fibro_tum = ascat.runAscat(ascat.bc_fibro_tum, gamma = 0.55, img.dir = args[7])
 segment_file_fibro <-ascat.output_fibro_tum$segments
 segment_file_fibro$aberrant_cell_fraction <- ascat.output_fibro_tum$aberrantcellfraction
 write.table(segment_file_fibro, args[7], sep="\t", col.names = TRUE, row.names = FALSE, quote = FALSE)
